fc56b3293e3794972f4759eeb7b01145e09c2695
Rael Max <rael.nascimento@corp.globo.com>
2017-06-20 09:29:30 -0300

    Add preview page annd update form to send data to correct view

--- certbrite/certbrite.py
+++ certbrite/certbrite.py
@@ -1,8 +1,16 @@
 from flask import Flask
+from flask import request
 from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+@app.route('/preview', methods=['POST'])
+def preview():
+    certificate_text = request.form.get('certificate-text', '')
+    certificate_theme = request.form.get('certificate-theme', 'theme-1')
+    return render_template('preview.html', text=certificate_text,
+                                           theme=certificate_theme)
