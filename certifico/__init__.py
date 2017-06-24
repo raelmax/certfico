cadaf9f9a4a020763f3b1aa17fa1b17d716148f6
Rael Max <contato@raelmax.com>
2017-06-23 23:51:39 -0300

    Add sendgrid and synchronous mail sending

--- certifico/__init__.py
+++ certifico/__init__.py
@@ -1,12 +1,17 @@
 import os
 from flask import Flask
 from flask_pymongo import PyMongo
+from flask_sendgrid import SendGrid
 
 app = Flask(__name__)
 
 if os.environ.get('MONGODB_URI'):
     app.config['MONGO_URI'] = os.environ.get('MONGODB_URI')
 
+app.config['SENDGRID_API_KEY'] = os.environ.get('SENDGRID_API_KEY')
+app.config['SENDGRID_DEFAULT_FROM'] = 'contato@raelmax.com'
+
+mail = SendGrid(app)
 mongo = PyMongo(app)
 
 import certifico.views
