8b482c2d341598fd8531b0fe62cf800b557ba76c
Rael Max <rael.nascimento@corp.globo.com>
2017-06-23 22:50:04 -0300

    Add mongo uri from enviroment variable

--- certifico/__init__.py
+++ certifico/__init__.py
@@ -1,7 +1,12 @@
+import os
 from flask import Flask
 from flask_pymongo import PyMongo
 
 app = Flask(__name__)
+
+if os.environ.get('MONGODB_URI'):
+    app.config['MONGO_URI'] = os.environ.get('MONGODB_URI')
+
 mongo = PyMongo(app)
 
 import certifico.views
