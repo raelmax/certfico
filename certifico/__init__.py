fc11afa57cb21ef84203538f2ee68aaf6cdaa8c4
Rael Max <contato@raelmax.com>
2017-06-27 09:55:56 -0300

    Simplify app config and mail worker

--- certifico/__init__.py
+++ certifico/__init__.py
@@ -1,20 +1,20 @@
 import os
+import redis
 
 from rq import Queue
 
 from flask import Flask
 from flask_pymongo import PyMongo
 
-from certifico.worker import conn
-
 app = Flask(__name__)
 
-if os.environ.get('MONGODB_URI'):
-    app.config['MONGO_URI'] = os.environ.get('MONGODB_URI')
-
 app.config['SENDGRID_API_KEY'] = os.getenv('SENDGRID_API_KEY')
+app.config['MONGO_URI'] = os.getenv('MONGODB_URI', 'mongodb://localhost:27017')
+app.config['REDIS_URI'] = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 
 mongo = PyMongo(app)
-redis_queue = Queue(connection=conn)
+
+redis_connection = redis.from_url(app.config.get('REDIS_URI'))
+redis_queue = Queue(connection=redis_connection)
 
 import certifico.views
