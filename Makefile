9f3e188bb40c7592993df9da5441e614c31ec8b8
Rael Max <contato@raelmax.com>
2017-07-25 18:15:17 -0300

    Add test_watch command to Makefile and field tests to forms

--- Makefile
+++ Makefile
@@ -1,3 +1,5 @@
+KQWAIT := $(shell command -v kqwait 2> /dev/null)
+
 run:
 	FLASK_APP=certifico FLASK_DEBUG=True flask run
 
@@ -6,3 +8,10 @@ setup:
 
 test:
 	MONGODB_URI=mongodb://localhost:27017/certifico_testing_db python -m unittest
+
+test_watch:
+ifdef KQWAIT
+	while true; do kqwait tests; make test; done
+else
+	@echo "You need kqwait installed to run this command. :/"
+endif
--- tests/test_forms.py
+++ tests/test_forms.py
@@ -8,14 +8,27 @@ class CertificateFormTestCase(TestCase):
         app.config['TESTING'] = True
         app.config['WTF_CSRF_ENABLED'] = False
 
+    def test_should_have_a_filefield_to_logo(self):
+        with app.app_context():
+            form = CertificateForm()
+            self.assertEqual(form.logo_file.type, 'FileField')
+
+    def test_should_have_a_hiddenfield_to_logo_value(self):
+        with app.app_context():
+            form = CertificateForm()
+            self.assertEqual(form.logo.type, 'HiddenField')
+
+    def test_should_have_a_textarea_to_message(self):
+        with app.app_context():
+            form = CertificateForm()
+            self.assertEqual(form.message.type, 'TextAreaField')
+
+    def test_should_have_a_textarea_to_participants(self):
+        with app.app_context():
+            form = CertificateForm()
+            self.assertEqual(form.participants.type, 'TextAreaField')
+
     def test_should_not_validate_empty_form(self):
         with app.app_context():
             form = CertificateForm()
-            self.assertFalse(form.validate_on_submit())
-
-    def test_should_have_a_file_field_to_certificate_logo(self):
-        with app.app_context():
-            form = CertificateForm()
-            self.assertFalse(form.validate_on_submit())
-            self.assertTrue(form.logo.type, 'FileField')
-
+            self.assertFalse(form.validate())
